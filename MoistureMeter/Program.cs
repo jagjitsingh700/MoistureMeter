6902e528ead25fccb7b554b0016b9d26f9fa2f4d
jagjitsingh700 <42186012+jagjitsingh700@users.noreply.github.com>
2020-08-24 08:00:16 +0200

    Add files via upload

---
+++ MoistureMeter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoistureMeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
