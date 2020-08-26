aabfbf031e2cb96fc465cd9b97ba33c7e6648d1d
JagjitSingh <jagjit.singh@hr-manager.net>
2020-08-26 07:41:28 +0200

    Added Microsoft logging framework

--- MoistureMeter/Program.cs
+++ MoistureMeter/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MoistureMeter
 {
@@ -6,7 +7,20 @@ namespace MoistureMeter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            logger.LogInformation("Moisture Meter Reading Activated");
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole())
+                    .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information);
         }
     }
 }
